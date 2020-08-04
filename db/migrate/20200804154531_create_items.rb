48322947c017b7d62b1e5ba021fb3439bff9f1e9
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-05 00:55:36 +0900

    items モデル作成

---
+++ db/migrate/20200804154531_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.integer :user_id
+      t.string  :item_name
+      t.text    :item_text
+      t.string  :category
+      t.string  :item_condition
+      t.text    :item_img
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_130828) do
+ActiveRecord::Schema.define(version: 2020_08_04_154531) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "item_name"
+    t.text "item_text"
+    t.string "category"
+    t.string "item_condition"
+    t.text "item_img"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
