3d1d43ce823c4aa21ebc5416926a443acf0d20d6
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-07 16:01:44 +0900

    Faker 詳細データ　実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     nick_name   { Faker::Name.initials( number: 4 ) }
     family_name { Faker::Name.last_name }
     first_name  { Faker::Name.first_name }
+    family_kana { Faker::Japanese::Name.last_name }
+    first_kana  { Faker::Japanese::Name.first_name }
+    email       { Faker::Internet.free_email }
+    password    { Faker::Internet.password( min_length: 6 ) }
+    birth_day   { Faker::Date.between( from: "1930-01-01", to: "2015-12-31" ) }
   end
 end
