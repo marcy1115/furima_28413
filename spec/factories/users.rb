8a5be765ced0ce337bfc8342b2bfd267aee15f38
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-10 13:46:13 +0900

    devise実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :user do
-    nick_name             { Faker::Name.initials( number: 4 ) }
-    family_name           { Faker::Name.last_name }
-    first_name            { Faker::Name.first_name }
-    family_kana           { Faker::Japanese::Name.last_name }
-    first_kana            { Faker::Japanese::Name.first_name }
-    email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password( min_length: 6 ) }
-    passWord_confirmation { password }
-    birth_day             { Faker::Date.between( from: "1930-01-01", to: "2015-12-31" ) }
+    
   end
 end
