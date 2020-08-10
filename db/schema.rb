54e8812dfd0cc6fef6180531c99341d3c42084c6
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-10 13:56:24 +0900

    userテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_10_044428) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nick_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_kana", null: false
-    t.string "first_kana", null: false
     t.string "email", default: "", null: false
-    t.date "birth_day", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
