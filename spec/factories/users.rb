1fbe1c7fda8b1734651adb0108528e7b1c32f784
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-10 19:58:24 +0900

    FactotyBot  詳細

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nick_name       { Faker::Name.initials( number: 4 ) }
+    first_name      { 'あア亜' }
+    last_name       { 'いイ井' }
+    first_name_kana { 'アアア' }
+    last_name_kana  { 'イイイ' }
+    email           { Faker::Internet.free_email }
+    password        { Faker::Internet.password( min_length: 6 ) }
+    birthday        { Faker::Date.between( from: '1930-01-01', to: '2015-12-31') }
   end
 end
