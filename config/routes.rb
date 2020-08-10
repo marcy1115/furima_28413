21071443954ddddba187fdb43c85930ceb62945f
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-10 20:31:24 +0900

    コントローラ名修正 itemからitems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get "users/sign_up", to: "dedevise/registrations#new"
-  resources :users
+  root 'items#index'
+  get "users/sign_up", to: "devise/registrations#new"
+  resources :users, only: [ :new, :create ]
+  resources :items, only: [ :index ]
 end
