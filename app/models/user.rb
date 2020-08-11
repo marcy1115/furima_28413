67100f20d7b3dcc21bec83ae6ed5965929d9f547
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-11 16:07:30 +0900

    ユーザー管理機能テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    validates :nick_name
+    validates :email, format: { with: /@.+/ }
+    validates :birth_day
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
+    validates :password
+    validates :password_confirmation
+  end
+
   
-
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nick_name       { Faker::Name.initials( number: 4 ) }
-    first_name      { 'あア亜' }
-    last_name       { 'いイ井' }
-    first_name_kana { 'アアア' }
-    last_name_kana  { 'イイイ' }
-    email           { Faker::Internet.free_email }
-    password        { Faker::Internet.password( min_length: 6 ) }
-    birthday        { Faker::Date.between( from: '1930-01-01', to: '2015-12-31') }
+    nick_name             { Faker::Name.initials( number: 4 ) }
+    first_name            { 'あア亜' }
+    last_name             { 'いイ井' }
+    first_name_kana       { 'アアア' }
+    last_name_kana        { 'イイイ' }
+    email                 { 'a@a' }
+    password              { '123abc' }
+    password_confirmation { password }
+    birth_day             { Faker::Date.between( from: '1930-01-01', to: '2015-12-31') }
   end
 end
