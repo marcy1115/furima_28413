a8f5c7966974bc35bd3a520234144341942aad0b
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-12 11:36:49 +0900

    item データベース　作成

--- db/migrate/20200812022803_create_items.rb
+++ db/migrate/20200812022803_create_items.rb
@@ -1,7 +1,11 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.integer    :user_id, null: false, foreign_key: true
+      t.string     :name,    null: false
+      t.text       :text,    null: false
+      t.text       :image,   null: false
+      t.integer    :price,   null: false
       t.timestamps
     end
   end
