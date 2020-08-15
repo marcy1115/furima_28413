027f7b24809719e67d8e62165ec6ff2d12f97bbc
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-15 16:02:15 +0900

    出品　ルーティング実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   get "users/sign_up", to: "devise/registrations#new"
+  post "/items/new", to: "items#new"
   resources :users, only: [ :new ]
   resources :items, only: [ :index, :new, :create ]
 end
