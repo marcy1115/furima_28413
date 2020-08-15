2f4b04a15841602650d55f5ae8572a51c41647c6
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-15 18:19:19 +0900

    ログイン有無　遷移変更実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_login, expect: [ :index, :new, :show ]
+  before_action :move_to_login, only: [ :new, :edit, :update, :destroy ]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,10 +20,11 @@ class ItemsController < ApplicationController
                                   :category_id, :quality_id, :burden_id,
                                   :prefecture_id, :delivery_id )
   end
-  # def move_to_login
-  #   if user_signed_in?
-  #     redirect_to new_user_session_path
-  #   end
-  # end
+
+  def move_to_login
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 
 end
