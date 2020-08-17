cf6db9200cc69b29429c3579e4801ad1311c3bbe
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-17 13:30:40 +0900

    user validation 変数格納

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  domain                  = /@.+/
+  jp_lang                 = /\A[ぁ-んァ-ン一-龥]+\z/
+  jp_lang_kana            = /\A[ァ-ン]+\z/
+  half_width_alphanumeric = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+
   with_options presence: true do
     validates :nick_name
-    validates :email, format: { with: /@.+/ }
+    validates :email, format: { with: domain }
     validates :birth_day
   end
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+  with_options presence: true, format: { with: jp_lang } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+  with_options presence: true, format: { with: jp_lang_kana } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
-  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } do
+  with_options presence: true, format: { with: half_width_alphanumeric } do
     validates :password
     validates :password_confirmation
   end
