cdd0f7dac5a8251dac8a6cda0da7e4dad0c0c1c9
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-17 14:41:35 +0900

    コメントアウト削除、validation修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,12 +6,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery
 
-  validates :user_id, presence: true
-
   with_options presence: true do
-    validates :name,  length: { maximum: 40 }
-    validates :text,  length: { maximum: 1000 }
-    validates :price, numericality: { greater_than_or_equal_to: 300,
+    validates :user_id
+    validates :name,   length: { maximum: 40 }
+    validates :text,   length: { maximum: 1000 }
+    validates :price,  numericality: { greater_than_or_equal_to: 300,
                                       less_than_or_equal_to: 9_999_999 }
   end
 
