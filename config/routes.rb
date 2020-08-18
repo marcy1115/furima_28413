fd1a4cbe22e72cfff7e88145c4763d9a99e06dfe
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-08-18 17:02:03 +0900

    詳細画面遷移実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   get  "users/sign_up", to: "devise/registrations#new"
-  get "/items/new",    to: "items#new"
+  get "items/new",    to: "items#new"
   
+
   resources :users, only: [ :new ]
-  resources :items, only: [ :index, :new, :create ]
+  resources :items, only: [ :index, :new, :create]
+  resources :items do
+    member do
+      get "item", to: "items#show"
+    end
+  end 
 end
