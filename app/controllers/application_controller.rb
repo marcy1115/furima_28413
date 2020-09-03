2e0dbe0835771ed140110b69e35f248218ccb304
marcy1115 <68366327+marcy1115@users.noreply.github.com>
2020-09-03 12:43:20 +0900

    Basic認証　実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
       :sign_up, keys: [:nick_name, :first_name, :last_name,
                        :first_name_kana, :last_name_kana, :birth_day]
     )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
